cf511a88b317cc3b51215e206989db74e389814b
Nahuel Balsamo <nbalsamo@elementum.com>
2019-10-08 18:51:37 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Paso 1: Escribir una funcion que se ejecuta cuando el DOM termino de cargar por completo. 
+// https://api.jquery.com/ready/
+
+// Paso 2: Seleccionar con jQuery al elemento del dron
+
+// Paso 3: Manipular el evento "mousemove" sobre todo el documento para que actualice 
+// la posicion "left" y "top" del  css del elemento del dron para que persiga 
+// la posicion actual del mouse.
+// https://api.jquery.com/mousemove/
+
+// Paso 4: Realizar los cambios necesarios para que el dron persiga la posicion actual
+// del mouse solo si se esta manteniendo presionado el click izquierdo.
+// https://api.jquery.com/mousedown/
+// https://api.jquery.com/mouseup/
